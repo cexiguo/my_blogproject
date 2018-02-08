82097b6a9c32cdc43dc249e16b7c39fef1a98444
loser-for-dream <1744865942@qq.com>
2018-02-08 13:45:06 +0800

    显示阅读量

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,6 @@
 import markdown
 from django.shortcuts import render, get_object_or_404
-from .models import Post,Category
+from .models import Post, Category
 from django.http import HttpResponse
 from comments.forms import CommentForm
 
@@ -14,6 +14,7 @@ def index(request):
 
 def detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
+    post.increase_views()
     post.body = markdown.markdown(post.body,
                                   extensions=[
                                       'markdown.extensions.extra',
@@ -22,15 +23,17 @@ def detail(request, pk):
                                   ])
     form = CommentForm()
     comment_list = post.comment_set.all()
-    context = {'post':post,'form':form,'comment_list':comment_list}
+    context = {'post': post, 'form': form, 'comment_list': comment_list}
     return render(request, 'blog/detail.html', context=context)
 
-def archives(request,year,month):
-  post_list = Post.objects.filter(create_time__year=year,create_time__month=month).order_by('-create_time')
-  return render(request,'blog/index.html',context={'post_list':post_list})
+
+def archives(request, year, month):
+    post_list = Post.objects.filter(
+    create_time__year=year, create_time__month=month).order_by('-create_time')
+    return render(request, 'blog/index.html', context={'post_list': post_list})
 
 
-def category(request,pk):
-  cate = get_object_or_404(Category,pk=pk)
-  post_list = Post.objects.filter(category=cate).order_by('-create_time')
-  return render(request,'blog/index.html',context={'post_list':post_list})
+def category(request, pk):
+    cate = get_object_or_404(Category, pk=pk)
+    post_list = Post.objects.filter(category=cate).order_by('-create_time')
+    return render(request, 'blog/index.html', context={'post_list': post_list})
