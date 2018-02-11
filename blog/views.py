4344261f21bf94b6fffaa3f636841dcc7cd2565f
loser-for-dream <1744865942@qq.com>
2018-02-11 18:57:52 +0800

    增加类的通用视图

--- blog/views.py
+++ blog/views.py
@@ -3,13 +3,20 @@ from django.shortcuts import render, get_object_or_404
 from .models import Post, Category
 from django.http import HttpResponse
 from comments.forms import CommentForm
+from django.views.generic import ListView
 
 # Create your views here.
 
 
-def index(request):
-    post_list = Post.objects.all().order_by('-create_time')
-    return render(request, 'blog/index.html', context={'post_list': post_list})
+class IndexView(ListView):
+    model = Post
+    template_name = 'blog/index.html'
+    context_object_name = 'post_list'
+
+
+# def index(request):
+#     post_list = Post.objects.all().order_by('-create_time')
+#     return render(request, 'blog/index.html', context={'post_list': post_list})
 
 
 def detail(request, pk):
@@ -27,13 +34,20 @@ def detail(request, pk):
     return render(request, 'blog/detail.html', context=context)
 
 
-def archives(request, year, month):
-    post_list = Post.objects.filter(
-    create_time__year=year, create_time__month=month).order_by('-create_time')
-    return render(request, 'blog/index.html', context={'post_list': post_list})
 
+#def archives(request, year, month):
+#    post_list = Post.objects.filter(
+#        create_time__year=year, create_time__month=month).order_by('-create_time')
+#    return render(request, 'blog/index.html', context={'post_list': post_list})
 
-def category(request, pk):
-    cate = get_object_or_404(Category, pk=pk)
-    post_list = Post.objects.filter(category=cate).order_by('-create_time')
-    return render(request, 'blog/index.html', context={'post_list': post_list})
+class ArchivesView(IndexView):
+	pass
+# def category(request, pk):
+#     cate = get_object_or_404(Category, pk=pk)
+#     post_list = Post.objects.filter(category=cate).order_by('-create_time')
+#     return render(request, 'blog/index.html', context={'post_list': post_list})
+
+class CategoryView(IndexView):
+    def get_queryset(self):
+   	 cate = get_object_or_404(Category, pk=self.kwargs.get('pk'))
+   	 return super(CategoryView, self).get_queryset().filter(category=cate)
