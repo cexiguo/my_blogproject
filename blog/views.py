49b6074ad726b5513125500c1b1e081711d1cda6
loser-for-dream <1744865942@qq.com>
2018-02-13 19:16:42 +0800

    添加完善分页

--- blog/views.py
+++ blog/views.py
@@ -3,8 +3,8 @@ from django.shortcuts import render, get_object_or_404
 from .models import Post, Category
 from django.http import HttpResponse
 from comments.forms import CommentForm
-from django.views.generic import ListView,DetailView
-from django.core.paginator import Paginator,EmptyPage,PageNotAnInteger
+from django.views.generic import ListView, DetailView
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 # Create your views here.
 
@@ -13,64 +13,126 @@ class IndexView(ListView):
     model = Post
     template_name = 'blog/index.html'
     context_object_name = 'post_list'
-    paginate_by =  2
+    paginate_by = 2
+
+    def get_context_data(self, **kwargs):
+      context = super().get_context_data(**kwargs)
+      paginator = context.get('paginator')
+      page = context.get('page_obj')
+      is_paginated = context.get('is_paginated')
+      pagination_data = self.pagination_data(paginator, page, is_paginated)
+      context.update(pagination_data)
+      return context
+   def pagination_data(self,paginator,page,is_paginated):
+    if not is_paginated:
+      return {}
+      left  =  []
+      right = []
+      right _has_more = False
+      left_has_more = False
+      first = False
+      last = False
+      page_number = page.number
+      total_pages = paginator.num_pages
+      page_range = paginator.page_range
+
+      if page_number==1:
+        right = page_range[page_number:page_number+2]
+        if right[-1] < total_pages - 1:
+              right_has_more = True
+
+          if right[-1] < total_pages:
+              last = True
+
+      elif page_number == total_pages:
+
+          left = page_range[(page_number - 3) if (page_number - 3) > 0 else 0:page_number - 1]
 
 
-# def index(request):
-#     post_list = Post.objects.all().order_by('-create_time')
-#     return render(request, 'blog/index.html', context={'post_list': post_list})
+          if left[0] > 2:
+              left_has_more = True
 
+          if left[0] > 1:
+              first = True
+      else:
 
-# def detail(request, pk):
-#     post = get_object_or_404(Post, pk=pk)
-#     post.increase_views()
-#     post.body = markdown.markdown(post.body,
-#                                   extensions=[
-#                                       'markdown.extensions.extra',
-#                                       'markdown.extensions.codehilite',
-#                                       'markdown.extensions.toc',
-#                                   ])
-#     form = CommentForm()
-#     comment_list = post.comment_set.all()
-#     context = {'post': post, 'form': form, 'comment_list': comment_list}
-#     return render(request, 'blog/detail.html', context=context)
+          left = page_range[(page_number - 3) if (page_number - 3) > 0 else 0:page_number - 1]
+          right = page_range[page_number:page_number + 2]
+
+          if right[-1] < total_pages - 1:
+              right_has_more = True
+          if right[-1] < total_pages:
+              last = True
+
+          if left[0] > 2:
+              left_has_more = True
+          if left[0] > 1:
+              first = True
+
+      data = {
+          'left': left,
+          'right': right,
+          'left_has_more': left_has_more,
+          'right_has_more': right_has_more,
+          'first': first,
+          'last': last,
+      }
+
+      return data
+        # def index(request):
+        #     post_list = Post.objects.all().order_by('-create_time')
+        #     return render(request, 'blog/index.html', context={'post_list': post_list})
+
+        # def detail(request, pk):
+        #     post = get_object_or_404(Post, pk=pk)
+        #     post.increase_views()
+        #     post.body = markdown.markdown(post.body,
+        #                                   extensions=[
+        #                                       'markdown.extensions.extra',
+        #                                       'markdown.extensions.codehilite',
+        #                                       'markdown.extensions.toc',
+        #                                   ])
+        #     form = CommentForm()
+        #     comment_list = post.comment_set.all()
+        #     context = {'post': post, 'form': form, 'comment_list': comment_list}
+        #     return render(request, 'blog/detail.html', context=context)
+
 
 class PostDetailView(DetailView):
     model = Post
     template_name = 'blog/detail.html'
     context_object_name = 'post'
 
-    def get(self,request,*args,**kwargs):
-      response = super(PostDetailView,self).get(request,*args,**kwargs)
-      self.object.increase_views()
-      return response
+    def get(self, request, *args, **kwargs):
+        response = super(PostDetailView, self).get(request, *args, **kwargs)
+        self.object.increase_views()
+        return response
 
-    def get_object(self,queryset=None):
-      post = super (PostDetailView,self).get_object(queryset=None)
-      post.body = markdown.markdown(post.body,
-                                    extensions=[
-                                    'markdown.extensions.extra'
-                                    'markdown.extensions.codehilite'
-                                    'markdown.extensions.toc'
-                                    ]
-                                    )
-      return post
+    def get_object(self, queryset=None):
+        post = super(PostDetailView, self).get_object(queryset=None)
+        post.body = markdown.markdown(post.body,
+                                      extensions=[
+                                          'markdown.extensions.extra'
+                                          'markdown.extensions.codehilite'
+                                          'markdown.extensions.toc'
+                                      ]
+                                      )
+        return post
 
-
-
-    def get_context_data(self,**kwargs):
-      context = super(PostDetailView,self).get_context_data(**kwargs)
-      form = CommentForm()
-      comment_list  = self.object.comment_set.all()
-      contex.update({
-                    'form':form,
-                    'comment_list':comment_list
-                    })
-      return context
+    def get_context_data(self, **kwargs):
+        context = super(PostDetailView, self).get_context_data(**kwargs)
+        form = CommentForm()
+        comment_list = self.object.comment_set.all()
+        contex.update({
+                      'form': form,
+                      'comment_list': comment_list
+                      })
+        return context
 # def archives(request, year, month):
 #    post_list = Post.objects.filter(
 #        create_time__year=year, create_time__month=month).order_by('-create_time')
 #    return render(request, 'blog/index.html', context={'post_list': post_list})
+
 
 class ArchivesView(IndexView):
     pass
