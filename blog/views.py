517fa1aea107c21832cdc0dec6b81403feaa62a1
loser-for-dream <1744865942@qq.com>
2018-02-14 15:14:52 +0800

    fix

--- blog/views.py
+++ blog/views.py
@@ -16,13 +16,13 @@ class IndexView(ListView):
     paginate_by = 2
 
     def get_context_data(self, **kwargs):
-      context = super().get_context_data(**kwargs)
-      paginator = context.get('paginator')
-      page = context.get('page_obj')
-      is_paginated = context.get('is_paginated')
-      pagination_data = self.pagination_data(paginator, page, is_paginated)
-      context.update(pagination_data)
-      return context
+        context = super().get_context_data(**kwargs)
+        paginator = context.get('paginator')
+        page = context.get('page_obj')
+        is_paginated = context.get('is_paginated')
+        pagination_data = self.pagination_data(paginator, page, is_paginated)
+        context.update(pagination_data)
+        return context
 
     def pagination_data(self, paginator, page, is_paginated):
         if not is_paginated:
@@ -38,20 +38,22 @@ class IndexView(ListView):
         page_range = paginator.page_range
 
         if page_number == 1:
-         right = page_range[page_number:page_number + 2]
-          if right[-1] < total_pages - 1:
+            right = page_range[page_number:page_number + 2]
+            if right[-1] < total_pages - 1:
                 right_has_more = True
 
             if right[-1] < total_pages:
                 last = True
         elif page_number == total_pages:
-            left = page_range[(page_number - 3) if (page_number - 3) > 0 else 0:page_number - 1]
+            left = page_range[(page_number - 3)
+                              if (page_number - 3) > 0 else 0:page_number - 1]
             if left[0] > 2:
                 left_has_more = True
             if left[0] > 1:
                 first = True
         else:
-            left = page_range[(page_number - 3) if (page_number - 3) > 0 else 0:page_number - 1]
+            left = page_range[(page_number - 3)
+                              if (page_number - 3) > 0 else 0:page_number - 1]
             right = page_range[page_number:page_number + 2]
 
             if right[-1] < total_pages - 1:
