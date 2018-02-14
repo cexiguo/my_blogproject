c58070f70024c393b73f812fec5c8e32a45b6c1f
loser-for-dream <1744865942@qq.com>
2018-02-14 12:22:05 +0800

    分页设置

--- blog/views.py
+++ blog/views.py
@@ -24,61 +24,61 @@ class IndexView(ListView):
       context.update(pagination_data)
       return context
    def pagination_data(self,paginator,page,is_paginated):
-    if not is_paginated:
-      return {}
-      left  =  []
-      right = []
-      right _has_more = False
-      left_has_more = False
-      first = False
-      last = False
-      page_number = page.number
-      total_pages = paginator.num_pages
-      page_range = paginator.page_range
+      if not is_paginated:
+        return {}
+        left  =  []
+        right = []
+        right _has_more = False
+        left_has_more = False
+        first = False
+        last = False
+        page_number = page.number
+        total_pages = paginator.num_pages
+        page_range = paginator.page_range
 
-      if page_number==1:
-        right = page_range[page_number:page_number+2]
-        if right[-1] < total_pages - 1:
-              right_has_more = True
-
-          if right[-1] < total_pages:
-              last = True
-
-      elif page_number == total_pages:
-
-          left = page_range[(page_number - 3) if (page_number - 3) > 0 else 0:page_number - 1]
-
-
-          if left[0] > 2:
-              left_has_more = True
-
-          if left[0] > 1:
-              first = True
-      else:
-
-          left = page_range[(page_number - 3) if (page_number - 3) > 0 else 0:page_number - 1]
-          right = page_range[page_number:page_number + 2]
-
+        if page_number==1:
+         right = page_range[page_number:page_number+2]
           if right[-1] < total_pages - 1:
-              right_has_more = True
-          if right[-1] < total_pages:
-              last = True
+                right_has_more = True
 
-          if left[0] > 2:
-              left_has_more = True
-          if left[0] > 1:
-              first = True
+            if right[-1] < total_pages:
+                last = True
 
-      data = {
-          'left': left,
-          'right': right,
-          'left_has_more': left_has_more,
-          'right_has_more': right_has_more,
-          'first': first,
-          'last': last,
-      }
+        elif page_number == total_pages:
 
-      return data
+            left = page_range[(page_number - 3) if (page_number - 3) > 0 else 0:page_number - 1]
+
+
+            if left[0] > 2:
+                left_has_more = True
+
+            if left[0] > 1:
+                first = True
+        else:
+
+            left = page_range[(page_number - 3) if (page_number - 3) > 0 else 0:page_number - 1]
+            right = page_range[page_number:page_number + 2]
+
+            if right[-1] < total_pages - 1:
+                right_has_more = True
+            if right[-1] < total_pages:
+                last = True
+
+            if left[0] > 2:
+                left_has_more = True
+            if left[0] > 1:
+                first = True
+
+        data = {
+            'left': left,
+            'right': right,
+            'left_has_more': left_has_more,
+            'right_has_more': right_has_more,
+            'first': first,
+            'last': last,
+        }
+
+        return data
         # def index(request):
         #     post_list = Post.objects.all().order_by('-create_time')
         #     return render(request, 'blog/index.html', context={'post_list': post_list})
