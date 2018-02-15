339a8884f9d2c0436d68dfd59206ed77654501d4
loser-for-dream <1744865942@qq.com>
2018-02-15 10:09:51 +0800

    修改views

--- blog/views.py
+++ blog/views.py
@@ -75,9 +75,9 @@ class IndexView(ListView):
         }
         return data
 
-        # def index(request):
-        #     post_list = Post.objects.all().order_by('-create_time')
-        #     return render(request, 'blog/index.html', context={'post_list': post_list})
+    def index(request):
+        post_list = Post.objects.all().order_by('-create_time')
+        return render(request, 'blog/index.html', context={'post_list': post_list})
 
     def detail(request, pk):
         post = get_object_or_404(Post, pk=pk)
@@ -90,8 +90,10 @@ class IndexView(ListView):
                                       ])
         form = CommentForm()
         comment_list = post.comment_set.all()
-        context = {'post': post, 'form': form,
-                   'comment_list': comment_list}
+        context = {'post': post,
+                   'form': form,
+                   'comment_list': comment_list
+                   }
         return render(request, 'blog/detail.html', context=context)
 
 
@@ -125,18 +127,31 @@ class PostDetailView(DetailView):
                       'comment_list': comment_list
                       })
         return context
-# def archives(request, year, month):
-#    post_list = Post.objects.filter(
-#        create_time__year=year, create_time__month=month).order_by('-create_time')
-#    return render(request, 'blog/index.html', context={'post_list': post_list})
 
 
-class ArchivesView(IndexView):
-    pass
-# def category(request, pk):
-#     cate = get_object_or_404(Category, pk=pk)
-#     post_list = Post.objects.filter(category=cate).order_by('-create_time')
-#     return render(request, 'blog/index.html', context={'post_list': post_list})
+def archives(request, year, month):
+    post_list = Post.objects.filter(
+        create_time__year=year, create_time__month=month).order_by('-create_time')
+    return render(request, 'blog/index.html', context={'post_list': post_list})
+
+
+class ArchivesView(ListView):
+    model = Post
+    template_name = 'blog/index.html'
+    context_object_name = 'post_list'
+
+    def get_queryset(self):
+        year = self.kwargs.get('year')
+        month = self.kwargs.get('month')
+        return super(ArchivesView, self).get_queryset().filter(created_time__year=year,
+                                                               created_time__month=month
+                                                               )
+
+
+def category(request, pk):
+    cate = get_object_or_404(Category, pk=pk)
+    post_list = Post.objects.filter(category=cate).order_by('-create_time')
+    return render(request, 'blog/index.html', context={'post_list': post_list})
 
 
 class CategoryView(IndexView):
